2753a4b25282212618465341a3c3e5efba213613
elpvickey <perumalvigneshram@gmail.com>
2022-11-22 10:56:07 +0530

    Changes codes added

--- upsert.py
+++ upsert.py
@@ -6,10 +6,11 @@ db = client["test_learning"]
 
 col = db["test_collection"]
 
-# UPSERT is UPDATE + INSERT 
+# UPSERT is UPDATE + INSERT (IF CONDITION QUERY DATA IS AVAILABLE IT WILL BE UPDATED OR ITS NOT AVAILABLE INSERT THE VALUE)
 
 update_query = {"$set":{"Salary":"15 LPA","MailID":"vickeyram2@gmail.com"}}
 
-condition_query = {"name":"Vigneshram"}
+condition_query = {"name":"Vigneshram Perumal"}
 
-col.update(condition_query,update_query,upsert=True)
+x = col.update_many(condition_query,update_query,upsert=True)
+print(x.modified_count, "documents updated.")
