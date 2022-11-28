82065bf0fea6ff73626979fa6894ec88d8419a96
elpvickey <perumalvigneshram@gmail.com>
2022-11-28 12:18:43 +0530

    New changes

--- replace.py
+++ replace.py
@@ -14,3 +14,8 @@ condition_query = {"name":"Ram Perumal"}
 
 x = col.replace_one(condition_query,update_query,upsert=True)
 print(x.modified_count, "documents updated.")
+
+# findoneandReplace()
+update_query = {"name":"Ram Perumal","Experience":7,"Salary":"15 LPA","MailID":"vickeyram2@gmail.com"}
+y = col.find_one_and_replace(condition_query,update_query,upsert=True)
+print(y,'---------------')
